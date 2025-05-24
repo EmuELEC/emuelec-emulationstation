e41145a426730ad93c4289e6811ef8c990f12e9e
worstcase-scenario <iamspamalot@gmx.de>
2025-05-24 15:41:22 +0200

    added background task

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -962,12 +962,14 @@ void GuiMenu::createGamepadConfig(Window* window, GuiSettings* systemConfigurati
 
 	// Wiimote with IR-Sensorbar
 	gamepadConfiguration->addEntry(_("ACTIVATE WIIMOTE WITH IR-SENSORBAR"), false, [window] {
-    int result = system("/usr/bin/runwiimote.sh");
+    int result = system("/usr/bin/runwiimote.sh &");
     if(result == 0)
         window->pushGui(new GuiMsgBox(window, _("Wiimote activated."), _("OK")));
     else
         window->pushGui(new GuiMsgBox(window, _("Error while running script."), _("OK")));
 });
+
+
 
 
 
